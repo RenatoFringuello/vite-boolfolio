74e409d32cdfc68507a30b2d70c3f4a26b77ccda
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-02 19:54:20 +0100

    add store js

--- src/store.js
+++ src/store.js
@@ -2,21 +2,24 @@ import { reactive } from "vue"
 import axios from 'axios'
 
 export const store = reactive({
-    apiUrl : 'http://localhost:8000/api',
-    dataRaw:[],
-    data:[],
-    getData(location){
-        axios.get(this.apiUrl + location)
-             .then((response)=>{
-                  // console.log(response.data.result.data);
-                  this.dataRaw = response.data.result.data;
-             })
-             .catch((error)=>{
-                  console.error(error);
-             })
-             .then(()=>{
-                  // console.warn(this.dataRaw);
-                  this.data = this.dataRaw;
-             });
-    }
+   apiUrl : 'http://localhost:8000/api',
+   dataRaw:[],
+   data:[],
+   isLoaded: false,
+   getData(location){
+      this.isLoaded = false;
+      axios.get(this.apiUrl + location)
+            .then((response)=>{
+               // console.log(response.data.result.data);
+               this.dataRaw = response.data.result.data;
+            })
+            .catch((error)=>{
+               console.error(error);
+            })
+            .then(()=>{
+               // console.warn(this.dataRaw);
+               this.data = this.dataRaw;
+               this.isLoaded = true;
+            });
+   }
 });
