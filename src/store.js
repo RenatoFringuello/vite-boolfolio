f57bd0a68641d98aa78e023a1169ecde87c9a609
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-02 17:41:10 +0100

    get data  in data variable

--- src/store.js
+++ src/store.js
@@ -4,13 +4,19 @@ import axios from 'axios'
 export const store = reactive({
     apiUrl : 'http://localhost:8000/api',
     dataRaw:[],
+    data:[],
     getData(location){
         axios.get(this.apiUrl + location)
              .then((response)=>{
-                console.log(response);
+                  // console.log(response.data.result.data);
+                  this.dataRaw = response.data.result.data;
              })
              .catch((error)=>{
-                console.error(error);
+                  console.error(error);
              })
+             .then(()=>{
+                  // console.warn(this.dataRaw);
+                  this.data = this.dataRaw;
+             });
     }
 });
