b89e4ed362c3628410e68317b623203fd9799b5c
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-02 17:35:02 +0100

    add appmain component

---
+++ src/components/AppMain.vue
@@ -0,0 +1,24 @@
+<script>
+    import {store} from '../store';
+    export default {
+        name:'AppMain',
+        data() {
+            return {
+                
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<template>
+    <main>
+
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
