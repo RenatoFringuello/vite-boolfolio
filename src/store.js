6068627e4a96d6edaaaab8af5d0d8653a5183687
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-03 01:18:37 +0100

    add next and prev page functions

--- src/store.js
+++ src/store.js
@@ -5,20 +5,65 @@ export const store = reactive({
    apiUrl : 'http://localhost:8000/api',
    dataRaw:[],
    data:[],
+   prevPageLink :'',
+   isPrevAvailable: false,
+   nextPageLink :'',
+   isNextAvailable: true,
    isLoaded: false,
    getData(location){
       this.isLoaded = false;
       axios.get(this.apiUrl + location)
             .then((response)=>{
-               // console.log(response.data.result.data);
-               this.dataRaw = response.data.result.data;
+               // console.log(response.data.result);
+               this.dataRaw = response;
             })
             .catch((error)=>{
                console.error(error);
             })
             .then(()=>{
-               // console.warn(this.dataRaw);
-               this.data = this.dataRaw;
+               this.data = this.dataRaw.data.result.data;
+               // console.log(this.dataRaw.data.result);
+               this.prevPageLink = this.dataRaw.data.result.next_page_url;
+               this.nextPageLink = this.dataRaw.data.result.next_page_url;
+               this.isLoaded = true;
+            });
+   },
+   getNextPage(){
+      this.isLoaded = false;
+      axios.get(this.dataRaw.data.result.next_page_url)
+            .then((response)=>{
+               console.log(response.data.result);
+               this.dataRaw = response;
+            })
+            .catch((error)=>{
+               // console.error(error);
+            })
+            .then(()=>{
+               //set the next and prev page at every call
+               this.isNextAvailable = this.dataRaw.data.result.next_page_url != null;
+               this.isPrevAvailable = this.dataRaw.data.result.prev_page_url != null;
+               //get the data
+               this.data = this.dataRaw.data.result.data;
+               //set true when all the data is loaded
+               this.isLoaded = true;
+            });
+   },
+   getPrevPage(){
+      this.isLoaded = false;
+      axios.get(this.dataRaw.data.result.prev_page_url)
+            .then((response)=>{
+               this.dataRaw = response;
+            })
+            .catch((error)=>{
+               // console.error(error);
+            })
+            .then(()=>{
+               //set the next and prev page at every call
+               this.isPrevAvailable = this.dataRaw.data.result.prev_page_url != null;
+               this.isNextAvailable = this.dataRaw.data.result.next_page_url != null;
+               //get the data
+               this.data = this.dataRaw.data.result.data;
+               //set true when all the data is loaded
                this.isLoaded = true;
             });
    }
