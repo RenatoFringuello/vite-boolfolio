05e4c4e0b9ee4026543321800899295ca69c5850
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-04 10:58:03 +0100

    add project slug

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import AppHome from './pages/AppHome.vue';
 import AppProjects from './pages/AppProjects.vue';
+import AppProjectSlug from './pages/AppProjectSlug.vue';
 import AppAboutMe from './pages/AppAboutMe.vue';
 
 const router = createRouter({
@@ -16,6 +17,11 @@ const router = createRouter({
             path:'/projects',
             name:'projects',
             component: AppProjects
+        },
+        {
+            path:'/projects/:slug',
+            name:'project',
+            component: AppProjectSlug
         },
         {
             path:'/about-me',
