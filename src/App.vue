48b826e86cfe9d0e3b01111bf73446b2c288686f
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-04 10:57:41 +0100

    add loader

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,19 @@
   export default{
     components:{
       AppHeader,
+    },
+    data() {
+      return {
+        isPageAvailable : false,
+      }
+    },
+    methods:{
+        getDataAfter(n){
+            setTimeout(()=>{
+                this.isPageAvailable = true;
+            },n);
+            return this.isPageAvailable;
+        },
     }
   }
 </script>
@@ -11,10 +24,52 @@
 <template>
   <AppHeader/>
   <main class="container">
-    <router-view></router-view>
+    <div v-if="getDataAfter(500)" class="row g-3 py-3">
+      <router-view></router-view>
+    </div>
+    <div v-else class="loading-container d-flex justify-content-center align-items-center align-content-center">
+        <div class="m-auto">
+            <div class="loader"></div>
+        </div>
+    </div>
   </main>
 </template>
 
 <style lang="scss">
   @use './style/general.scss' as *;
+  @use './style/partials/variables' as *;
+
+  .loading-container{
+      height: calc(100vh - 80px);
+      .loader{
+          width: 8vmax;
+          height: 8vmax;
+          border-right: 4px solid $main-text-color;
+          border-radius: 100%;
+          animation: spinRight 800ms linear infinite;
+          
+          &:before, &:after {
+              content: '';
+              width: 6vmax;
+              height: 6vmax;
+              display: block;
+              position: absolute;
+              top: calc(50% - 3vmax);
+              left: calc(50% - 3vmax);
+              border-left: 3px solid $second-text-color;
+              border-radius: 100%;
+              animation: spinLeft 800ms linear infinite;
+          }
+          
+          &:after {
+              width: 4vmax;
+              height: 4vmax;
+              top: calc(50% - 2vmax);
+              left: calc(50% - 2vmax);
+              border: 0;
+              border-right: 2px solid $main-text-color;
+              animation: none;
+          }
+      }
+  }
 </style>
