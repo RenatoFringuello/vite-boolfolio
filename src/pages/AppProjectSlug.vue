8c69631162556e2c974f56b66faea1930746f73b
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-05 15:57:38 +0100

    add project component

---
+++ src/pages/AppProjectSlug.vue
@@ -0,0 +1,31 @@
+<script>
+  import {store} from '../store'
+  import ProjectComponent from '../components/mainComponents/ProjectComponent.vue';
+
+  export default{
+    name: "AppProjectSlug",
+    components: { 
+      ProjectComponent 
+    },
+    data() {
+      return {
+        store,
+      }
+    },
+    created(){
+      store.getData('/projects/'+this.$route.params.slug);
+    }
+  }
+</script>
+
+<template>
+  <ProjectComponent v-if="store.isLoaded" 
+                    :project="store.dataRaw.data.result"
+                    :baseUrl="store.dataRaw.data.baseStorageUrl"
+                    :isShow="true"
+  />
+</template>
+
+<style lang="scss">
+  @use '../style/general.scss' as *;
+</style>
